971788cd2b029033b33c2c3e347b6d3387ad7117
Brian Byers <brian@xclo.io>
2021-02-09 21:16:38 -0600

    initial

---
+++ src/main/java/com/vmware/k8s/k8stestapp/K8sTestAppApplication.java
@@ -0,0 +1,13 @@
+package com.vmware.k8s.k8stestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8sTestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K8sTestAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vmware/k8s/k8stestapp/K8sTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vmware.k8s.k8stestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sTestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
